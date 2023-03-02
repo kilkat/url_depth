4ca4dc8020382068c68a54910c74031db292e3b5
KILKAT_NOTEBOOK <rhkddns3459@naver.com>
2023-03-02 16:51:27 +0900

    2023-03-02_kilkat

--- url_fuzzer.py
+++ url_fuzzer.py
@@ -22,7 +22,7 @@ def bruteforce():
                     r_status = r_result.status_code
                     print(r_result)
                     if(r_status == 200):
-                        f1 = open("/url_fuzzer/find_output.txt", "w")
+                        f1 = open("find_output.txt", "w")
                         f1.write(r_result + " 탐지됨")
                         f1.close()
                         break
@@ -30,7 +30,7 @@ def bruteforce():
             except:
                 # for j in range(1, 3):
                 #     r_result = requests.get(url+ "/" + "".join(i))
-                f2 = open("/url_fuzzer/err_output.txt", "w")
+                f2 = open("err_output.txt", "w")
                 f2.write(r_result + " 충돌")
                 f2.close()
                 pass
