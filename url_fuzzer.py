18df2ec38f30592b151ed20d4d9db2c0b2659f27
kilkat <rhkddns3459@naver.com>
2023-03-02 22:38:11 +0900

    2023-03-02_kilkat

--- url_fuzzer.py
+++ url_fuzzer.py
@@ -17,27 +17,22 @@ def bruteforce():
             attempt = product(possibility, repeat=num)
             try:
                 for i in attempt:
-                    # print(i)
                     r_result = requests.get(url+ "/" + "".join(i))
-                    # r_status = r_result.status_code
                     print(r_result)
-                    # print(r_status)
                     if(r_result.status_code == 200):
-                        print("----------" + r_result.status_code + "----------")
                         f1 = open("find_output.txt", "a")
-                        data = r_result + " 탐지됨"
+                        data = str(r_result) + " detection\n"
                         f1.write(data)
                         f1.close()
-                        break
                 num += 1
             except:
                 for j in range(1, 3):
                     r_result = requests.get(url+ "/" + "".join(i))
                 pass
-                f2 = open("err_output.txt", "w")
-                f2.write(r_result + " 충돌 및 오류 발생")
+                f2 = open("err_output.txt", "a")
+                f2.write(str(r_result) + " crash or error\n")
                 f2.close()
     else:
-        print("탐지에 실패했습니다.")
+        print("detection fail")
 
 bruteforce()
